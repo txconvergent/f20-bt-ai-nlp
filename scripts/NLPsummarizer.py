bd96bca30ad7276629db7957c17698532dc2e762
akifabidi <akif.abidi@utexas.edu>
2020-11-24 19:48:52 -0600

    Update NLPsummarizer.py

--- scripts/NLPsummarizer.py
+++ scripts/NLPsummarizer.py
@@ -6,6 +6,8 @@ from firebase_admin import firestore
 import firebase_admin
 from firebase_admin import credentials
 from summarizer import Summarizer
+from newspaper import fulltext
+import requests
 
 
 cred = credentials.Certificate(r"C:\Users\akifa\OneDrive\Documents\GitHub\f20-bt-ai-nlp\scripts\forestoreKey.json")
@@ -14,13 +16,24 @@ db = firestore.client()
 
 # get all data from the unsummarized collection in db  
 
-def summarizeText():
+def main():
+    summarizeDailyTexan()
+    summarizeUTNews()
+
+if __name__ == "__main__":
+    main()
+
+
+def summarizeUTNews():
     all_data = db.collection(u'UTNews').document(u'Documents').collection(u'Unsummarized').get()
     # take text for each article, summarize it and add it to summarized collection
     #print('next is ', next(all_data))
     model = Summarizer()
+    # TODO: REMEMBER TO CHANGE THE RANGE BELOW
     for doc in all_data[:4]:
+        # gets raw, unsummarized article
         article =  doc.get('text')
+        # summarizes the articles into 2 sentences
         result = model(article, num_sentences = 2)
         summary = "".join(result)
         new_data = {
@@ -31,13 +44,28 @@ def summarizeText():
         db.collection(u'UTNews').document(u'Documents').collection(u'Summarized').add(new_data)
         print('----------finished--------')
 
-summarizeText()
+summarizeUTNews()
 
+# take text for each article, summarize it and add it to summarized collection
+def summarizeDailyTexan():
+    all_data = db.collection(u'DailyTexan').document(u'Documents').collection(u'Unsummarized').get()
 
-'''
-from newspaper import fulltext
-import requests
-article_url="https://thedailytexan.com/2020/11/09/ut-austin-student-government-petition-pass-fail"
-article = fulltext(requests.get(article_url).text)
-print(article)
-'''
+    model = Summarizer()
+    # TODO: REMEMBER TO CHANGE THE RANGE BELOW
+    for doc in all_data[:4]:
+        # uses newpaper library to get text of article from the URL
+        article = fulltext(requests.get(doc.get('URL')).text)
+        # summarizes the articles into 2 sentences
+        result = model(article, num_sentences = 2)
+        summary = "".join(result)
+        new_data = {
+            'title': doc.get('title'),
+            'url': doc.get('URL'),
+            'text': summary,
+            'genre': doc.get('genre')
+        }
+        db.collection(u'DailyTexan').document(u'Documents').collection(u'Summarized').add(new_data)
+        print('----------finished--------')
+
+summarizeUTNews()
+
